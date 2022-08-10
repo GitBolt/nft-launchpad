eef36afd54cdeacf89a75984c94a4d913e061481
GitBolt <saabisakhtar@gmail.com>
2022-08-11 02:10:13 +0530

    add: validations

---
+++ util/validation.ts
@@ -0,0 +1,16 @@
+export const isAlphaNumeric = (text: string, ignoreSpaces: boolean) => {
+  if (!text) return;
+  let regex;
+  if (ignoreSpaces) {
+    regex = /^[\w\-\s]+$/;
+  } else {
+    regex = /^[a-zA-Z\d-_]+$/;
+  }
+  return text.match(regex);
+};
+
+export const isDiscordInvite = (text: string) => {
+  if (!text) return;
+  const regex = /(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li)|discordapp\.com\/invite)\/.+[a-z]/;
+  return  text.match(regex);
+}; 
